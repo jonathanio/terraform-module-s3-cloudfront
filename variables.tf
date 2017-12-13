6510047ae7f79214416b028aa8619e342e35fa8a
Rafael Bernardo <rafael@faztodadiferenca.com.br>
2017-12-13 20:19:40 -0200

    included oficial documentation for parameter

--- variables.tf
+++ variables.tf
@@ -59,6 +59,6 @@ variable "tags" {
 }
 
 variable "force_destroy" {
-  description = "force destroy bucket even bucket is not empty"
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   default     = "false"
 }
