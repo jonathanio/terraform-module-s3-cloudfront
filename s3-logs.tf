e7503a048d1a6fa0f351c3e195acd407f93691f4
Howard Huang <h.huang65@gmail.com>
2020-08-19 16:33:21 -0700

    Update for Terraform 0.12 support
    This changes the syntax of the entire project to be compatible with
    Terraform 0.12.
    
    There are some fixes that just clear out deprecation warnings, and 1 fix
    that actually fixes an error that occurs because Terraform 0.12 kind of
    changed how `compact` and interpolation works when doing
    
    ```
    aliases = ["${compact(...)}"]
    ```
    
    vs
    
    ```
    aliases = compact(...)
    ```
    
    This is the `aws_cloudfront_distribution.website`'s `aliases` variable
    specifically.

--- s3-logs.tf
+++ s3-logs.tf
@@ -1,26 +1,26 @@
 resource "aws_s3_bucket" "logs" {
   bucket        = "s3-cloudfront-${lower(var.name)}-logs"
   acl           = "log-delivery-write"
-  force_destroy = "${var.force_destroy}"
+  force_destroy = var.force_destroy
 
   lifecycle_rule {
     id      = "s3-cloudfront-${lower(var.name)}-logs-transitions"
     enabled = true
 
     transition {
-      days          = "${var.logs_transition_ia}"
+      days          = var.logs_transition_ia
       storage_class = "STANDARD_IA"
     }
 
     transition {
-      days          = "${var.logs_transition_glacier}"
+      days          = var.logs_transition_glacier
       storage_class = "GLACIER"
     }
 
     expiration {
-      days = "${var.logs_expiration}"
+      days = var.logs_expiration
     }
   }
 
-  tags = "${merge(var.tags, map("Name", format("s3-cloudfront-%s-logs", var.name)))}"
+  tags = merge(var.tags, map("Name", format("s3-cloudfront-%s-logs", var.name)))
 }
